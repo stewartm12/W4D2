8e46d00a6432465a2314b83cb9cdca297a1da817
Brad Trick <btrick@gmail.com>
2021-01-19 14:56:52 -0500

    Fill out #move_piece and add #valid_move?

--- chess/board.rb
+++ chess/board.rb
@@ -18,11 +18,26 @@ class Board
   end
 
   def move_piece(start_pos, end_pos)
-    raise "No piece at that position" if self[pos] == nil
+    raise "No piece at that position" if self[start_pos] == nil
+    raise "Piece cannot be moved to that position" unless valid_move?(start_pos, end_pos) 
+
+    self[start_pos], self[end_pos] = nil, self[start_pos]
+    self[end_pos].pos = end_pos
   end
+
+  def valid_move?(start_pos, end_pos)
+    x, y = end_pos
+    return x.between?(0, 7) && y.between?(0,7)
+  end
+
 
   def [](pos)
     x, y = pos
     @board[x][y]
+  end
+
+  def []=(pos, value)
+    x, y = pos
+    @board[x][y] = value
   end
 end
