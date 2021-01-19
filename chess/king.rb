c645151dec4b12348d33c6b5b3accb773d98dc96
Brad Trick <btrick@gmail.com>
2021-01-19 16:59:59 -0500

    Add minor changes; stepable not working

--- chess/king.rb
+++ chess/king.rb
@@ -1,7 +1,10 @@
 require_relative "piece"
+require_relative "stepable"
 
 class King < Piece
 
+  include Stepable
+  
   def symbol 
     :K
   end
--- chess/knight.rb
+++ chess/knight.rb
@@ -1,7 +1,10 @@
 require_relative "piece"
+require_relative "stepable"
 
 class Knight < Piece
 
+  include Stepable
+  
   def symbol 
     :N
   end
